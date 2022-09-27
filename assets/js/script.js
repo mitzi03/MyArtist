cc26c727e58037dac86f2503a284747258db98d4
Skylar Dorn <skylarwdorn@gmail.com>
2022-09-27 13:52:54 -0700

    added api for artist background

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,2 @@
-console.log('hello world')
+// API for the artist background.
+// https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=usher&api_key=c530cc7409dd0b36d399647084a9eae3&format=json
