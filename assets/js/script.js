fb38c248d01dac0a91d8a4c1fb2c3cb51588d0e1
dorntrevor7 <dorntrevor7@gmail.com>
2022-09-28 14:12:22 -0700

    weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+// const $search = document.querySelector("#search").value;
+fetch(
+  "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.artists.get?q_artist=prodigy&page_size=5&apikey=bd82708062a53880d8766141cccacee6",
+  {
+    method: "GET",
+  }
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
