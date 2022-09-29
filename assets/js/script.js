d4772f7777f364f95977e00e47048c9fd6e9436d
dorntrevor7 <dorntrevor7@gmail.com>
2022-09-29 13:20:59 -0700

    trev func

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
+// Trev Code
+// function chartsApi() {
 // const $search = document.querySelector("#search").value;
 fetch(
-  "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.artists.get?q_artist=prodigy&page_size=5&apikey=bd82708062a53880d8766141cccacee6",
-  {
-    method: "GET",
-  }
+  "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.artists.get?q_artist=prodigy&page_size=5&apikey=bd82708062a53880d8766141cccacee6"
 )
-  .then(function (response) {
-    return response.json();
-  })
+  .then((response) => response.json())
   .then(function (data) {
     console.log(data);
+  })
+  .catch(function (err) {
+    console.log(err);
   });
+// }
+// trev code
