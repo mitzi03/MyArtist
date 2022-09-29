c7b744ffd297c7ac469bc67f18b4a020af92d6d3
Skylar Dorn <skylarwdorn@gmail.com>
2022-09-29 14:37:13 -0700

    building JS function for background info on artist

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@
 const $inputValEl = document.getElementById(""); // input value
 const $inputEl = document.getElementById("") // where the artist background page will load 
 
+
+
+
 function getArtistBackground() {
     let $artistBackgroundAPI =
       `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${$inputValEl}&api_key=c530cc7409dd0b36d399647084a9eae3&format=json`
@@ -19,9 +22,19 @@ function getArtistBackground() {
 
 var displayArtistBackground = function ($inputValEl) {
     if ($inputValEl.length === 0) {
-      $inputEl.textContent = 'No repositories found.';
+      $inputEl.textContent('') = ;
       return;
     }
+
+    for (var i = 0; i < $inputValEl.length; i++) {
+        var inputEl = document.createElement('p');
+        inputEl.classList = 'flex-row';
+        inputEl.setAttribute('href', issues[i].html_url);
+        inputEl.setAttribute('target', '_blank');
+
+}
+
+
 
 getArtistBackground();
 $btn.addEventListener("click", getArtistBackground);
