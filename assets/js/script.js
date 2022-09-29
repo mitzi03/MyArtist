ce6afafa86079d51191d0b793deb35e30d8c336b
Jake Schmidt <jakeschmidt51@gmail.com>
2022-09-29 11:33:05 -0700

    rearranged JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+console.log('hello world')
+const ticketMasterKey = 'oLCyT4thVDHoA3TZaUi4qbtkL1zuG8kn';
+let eventName;
+let date;
+let venue;
+function getEventInfo(){
+    fetch(`https://app.ticketmaster.com/discovery/v2/events.json?&classificationName=concert,music&sort=onSaleStartDate,asc&apikey=${ticketMasterKey}`)
+    .then((res) =>{
+        res.json().then((data) => {
+          console.log(data);
+          date = data._embedded.events[0].dates.start.localDate;
+          eventName=data._embedded.events[0].name;
+          venue= data._embedded.events[0]._embedded.venues[0].name;
+          console.log(date, eventName, venue);
+          // setEventInfo();
+        })
+      })
+    }
+    getEventInfo();
+    
+// function setEventInfo(){
+//     document.getElementById('events').innerHTML=eventName + date + venue;
+
+// }
 // API for the artist background.
  var $btn = document.getElementById("search"); // btn
 const $inputValEl = document.getElementById(""); // input value
